282c86fd6574d266971d321380f465538b069784
Damola Olutoke <olutokeezekiel18@gmail.com>
2022-02-24 14:25:40 +0100

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './components/SignIn';
 import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 
